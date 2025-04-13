c1d81cc8f8b11e7ea4f5cb67941c91a9e1243b63
Ikenna Paul Abonyi <75263399+IkennaPaul97@users.noreply.github.com>
2025-04-14 01:23:35 +0200

    Fix Jest tests for HealthController and HealthService

--- backend/src/health/health.controller.spec.ts
+++ backend/src/health/health.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { HealthMetric } from './models/health-metric.schema';
 
 describe('HealthController', () => {
   let controller: HealthController;
@@ -7,6 +10,13 @@ describe('HealthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
+      providers: [
+        HealthService,
+        {
+          provide: getModelToken(HealthMetric.name),
+          useValue: {}, // Mock Mongoose model
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
